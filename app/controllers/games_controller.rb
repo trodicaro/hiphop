c937a2b05b4a52aa3f5924653d94718123e363e7
Hana Worku <haworku@gmail.com>
2015-03-23 23:18:25 -0500

    created Game model no idea if I need this

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,15 @@
 class GamesController < ApplicationController
   include RapGenius::Client
-  
   attr_reader :chosen_song, :mid_num
 
-def initialize
+  def new
+    # @game = Game.new 
+    
     random = rand(1..100)
     @chosen_song = RapGenius::Song.find(random)
-    @mid_num = @chosen_song.lines.count/2 
-end
-
-  def new
- 
-    @sample_game = Game.new 
-  	# random = rand(1..100)
-  	# @chosen_song = RapGenius::Song.find(random)
-  	# @mid_num = @chosen_song.lines.count/2
+    @mid_num = @chosen_song.lines.count/2
   end
+  
 
   # def start
   # 	
@@ -27,7 +21,7 @@ end
   end
 
   def try2
-     @game
+     
   end
 
   def fail
