60b276b36537762e2c45fbbd88fcb797046d1455
Hana Worku <haworku@gmail.com>
2015-03-25 21:59:18 -0500

    button is working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   include RapGenius::Client
+  before_action :which_lyrics, except: [:create]
 
   def create
     @random= rand(1..100) #may want to change how random is selected
@@ -9,45 +10,35 @@ class GamesController < ApplicationController
   end
 
   def try1
-    @game = Game.find(params[:id])
-    @chosen_song = RapGenius::Song.find(@game.song_id)
-    @mid_num = @chosen_song.lines.count/2
+    
   end
 
   def try2
-  @game = Game.find(params[:id])
-    @chosen_song = RapGenius::Song.find(@game.song_id)
-    @mid_num = @chosen_song.lines.count/2
+  
   end
 
 
   def final
-    @game = Game.find(params[:id])
-    @chosen_song = RapGenius::Song.find(@game.song_id)
-    @mid_num = @chosen_song.lines.count/2
+   
   end
 
-  # def answer
-  #   if 
-  #   redirect_to success_game_path 
-  #   else
-  #   redirect_to fail_game_path
-  # end
   
-  # def fail
-  #   random = rand(1..100)
-  #   @chosen_song = RapGenius::Song.find(random)
-  #   @mid_num = @chosen_song.lines.count/2
-  # end
+  def fail
 
-  # def success
-# random = rand(1..100)
-#     @chosen_song = RapGenius::Song.find(random)
-#     @mid_num = @chosen_song.lines.count/2
-#   end
+  end
+
+  def success
+  end
 
   # def artist
   # @artist ||= RapGenius::Artist.find(params[:id])
   # end
+
+private
+  def which_lyrics
+    @game = Game.find(params[:id])
+    @chosen_song = RapGenius::Song.find(@game.song_id)
+    @mid_num = @chosen_song.lines.count/2
+  end
 
 end
